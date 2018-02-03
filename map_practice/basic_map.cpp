2b9b6f3740b014f4df095eaabb9fae7368d6dfe6
Yourong Ye <34338026+YourongYe@users.noreply.github.com>
2018-02-03 11:09:00 +0000

    Create basic_map.cpp

---
+++ map_practice/basic_map.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <utility>
+
+int main()
+{
+    std::map<int,std::string> m;
+    m.insert( std::pair<int,std::string>(9175,"Mincong") );
+    m.insert( std::pair<int,std::string>(1,"a") );
+    m.insert( std::pair<int,std::string>(4,"d") );
+    m.insert( std::pair<int,std::string>(3,"c") );
+    m.insert( std::pair<int,std::string>(9174,"Yourong") );
+    
+    for(std::map<int,std::string>::iterator it=m.begin(); it!=m.end(); ++it){
+        std::cout<< it->first <<" : "<< it->second<<std::endl;
+    }
+    
+}

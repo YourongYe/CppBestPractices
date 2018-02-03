98e52044f401ff144e8f04423e38a71ff3b4c71f
Yourong Ye <34338026+YourongYe@users.noreply.github.com>
2018-02-03 10:33:34 +0000

    Create basic_set.cpp

---
+++ set_practice/basic_set.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <set>
+
+int main()
+{
+    std::set<int> s;
+
+    s.insert(5);
+    s.insert(1);
+    s.insert(-2);
+    s.insert(10);
+    s.insert(3);
+    
+    for(std::set<int>::iterator it=s.begin(); it!=s.end(); ++it){
+        std::cout<<*it<<std::endl;
+    }
+}
